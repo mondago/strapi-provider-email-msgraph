dbf8b44512741b4a4ef15b2c6241cbacf064da11
Jonny Ingall <jonny@mondago.com>
2023-09-06 13:41:36 +0100

    Actually use the mondago prettier config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 /**
  * Module dependencies
@@ -7,91 +7,87 @@
 /* eslint-disable import/no-unresolved */
 /* eslint-disable prefer-template */
 // Public node modules.
-import "isomorphic-fetch";
-import { Client } from "@microsoft/microsoft-graph-client";
-import { ClientSecretCredential } from "@azure/identity";
-import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
+import 'isomorphic-fetch';
+import { Client } from '@microsoft/microsoft-graph-client';
+import { ClientSecretCredential } from '@azure/identity';
+import { TokenCredentialAuthenticationProvider } from '@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials';
 
 interface Settings {
-  defaultFrom: string;
-  defaultReplyTo: string;
+	defaultFrom: string;
+	defaultReplyTo: string;
 }
 
 interface SendOptions {
-  from?: string;
-  to: string;
-  cc: string;
-  bcc: string;
-  replyTo?: string;
-  subject: string;
-  text: string;
-  html: string;
-  [key: string]: unknown;
+	from?: string;
+	to: string;
+	cc: string;
+	bcc: string;
+	replyTo?: string;
+	subject: string;
+	text: string;
+	html: string;
+	[key: string]: unknown;
 }
 
 interface ProviderOptions {
-  tenantId: string;
-  clientId: string;
-  clientSecret: string;
+	tenantId: string;
+	clientId: string;
+	clientSecret: string;
 }
 
 export = {
-  provider: "msgraph",
-  name: "Microsoft Graph Email Plugin",
+	provider: 'msgraph',
+	name: 'Microsoft Graph Email Plugin',
 
-  init(providerOptions: ProviderOptions, settings: Settings) {
-    const authProvider = new TokenCredentialAuthenticationProvider(
-      new ClientSecretCredential(
-        providerOptions.tenantId,
-        providerOptions.clientId,
-        providerOptions.clientSecret,
-      ),
-      { scopes: ["https://graph.microsoft.com/.default"] },
-    );
+	init(providerOptions: ProviderOptions, settings: Settings) {
+		const authProvider = new TokenCredentialAuthenticationProvider(
+			new ClientSecretCredential(providerOptions.tenantId, providerOptions.clientId, providerOptions.clientSecret),
+			{ scopes: ['https://graph.microsoft.com/.default'] },
+		);
 
-    return {
-      send: async (options: SendOptions) => {
-        const getEmailFromAddress = () => {
-          if (!options.from) {
-            return settings.defaultFrom;
-          }
+		return {
+			send: async (options: SendOptions) => {
+				const getEmailFromAddress = () => {
+					if (!options.from) {
+						return settings.defaultFrom;
+					}
 
-          const regex = /[^< ]+(?=>)/g;
-          const matches = options.from.match(regex);
-          return matches?.length ? matches[0] : settings.defaultFrom;
-        };
+					const regex = /[^< ]+(?=>)/g;
+					const matches = options.from.match(regex);
+					return matches?.length ? matches[0] : settings.defaultFrom;
+				};
 
-        const client = Client.initWithMiddleware({
-          debugLogging: false,
-          authProvider: authProvider,
-        });
+				const client = Client.initWithMiddleware({
+					debugLogging: false,
+					authProvider: authProvider,
+				});
 
-        const from = getEmailFromAddress();
-        const mail = {
-          subject: options.subject,
-          from: {
-            emailAddress: { address: from },
-          },
-          toRecipients: [
-            {
-              emailAddress: {
-                address: options.to,
-              },
-            },
-          ],
-          body: options.html
-            ? {
-                content: options.html,
-                contentType: "html",
-              }
-            : {
-                content: options.text,
-                contentType: "text",
-              },
-        };
+				const from = getEmailFromAddress();
+				const mail = {
+					subject: options.subject,
+					from: {
+						emailAddress: { address: from },
+					},
+					toRecipients: [
+						{
+							emailAddress: {
+								address: options.to,
+							},
+						},
+					],
+					body: options.html
+						? {
+								content: options.html,
+								contentType: 'html',
+						  }
+						: {
+								content: options.text,
+								contentType: 'text',
+						  },
+				};
 
-        await client.api(`/users/${from}/sendMail`).post({ message: mail });
-      },
-    };
-  },
+				await client.api(`/users/${from}/sendMail`).post({ message: mail });
+			},
+		};
+	},
 };
